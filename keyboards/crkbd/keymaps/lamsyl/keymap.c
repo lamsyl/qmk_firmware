c7f321260f6879b41b0f3f671797021e6f121ec9
yuklam <lamsungdev@gmail.com>
2023-12-12 23:40:25 +0800

    iter 25: hold ` to send ctrl

--- keyboards/crkbd/keymaps/lamsyl/keymap.c
+++ keyboards/crkbd/keymaps/lamsyl/keymap.c
@@ -15,6 +15,7 @@ enum custom_layers {
 #endif
 
 #define OSM_CAG OSM(MOD_LCTL|MOD_LALT|MOD_LGUI)
+#define LT3GRV  LT(3, KC_GRV)
 
 enum custom_keycodes {
     CAP3 = SAFE_RANGE,  // Screenshot (Cmd+Shift+3), hold ctrl to copy to clipboard
@@ -111,7 +112,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   KC_GRV,
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   LT3GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,   KC_UP, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
