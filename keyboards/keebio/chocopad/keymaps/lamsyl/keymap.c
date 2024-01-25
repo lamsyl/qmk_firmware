be6c79cf92e47e5861603417dcdc9e3706b21a69
yuklam <lamsungdev@gmail.com>
2024-01-26 00:51:33 +0800

    iter 1: rails snippets

--- keyboards/keebio/chocopad/keymaps/lamsyl/keymap.c
+++ keyboards/keebio/chocopad/keymaps/lamsyl/keymap.c
@@ -6,13 +6,36 @@
 #define _BASE 0
 #define _FN1 1
 
+enum custom_keycodes {
+    NOQC = SAFE_RANGE,  // No Query Cache
+    RC,                 // Rails Console
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case NOQC:
+    if (record->event.pressed) {
+      SEND_STRING("Mongoid::QueryCache.enabled = false");
+    }
+    return false;
+
+  case RC:
+    if (record->event.pressed) {
+      SEND_STRING("OTEL_LOG_LEVEL=FATAL bundle exec rails c");
+    }
+    return false;
+  }
+
+  return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_ortho_4x4(
+    XXXXXXX,  NOQC,     RC,       XXXXXXX,
+    XXXXXXX,  XXXXXXX,  KC_R,     KC_BSPC,
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    MO(_FN1), XXXXXXX,  XXXXXXX,  XXXXXXX
+    MO(_FN1), KC_SPC,   KC_SPC,   KC_ENT
   ),
 
   [_FN1] = LAYOUT_ortho_4x4(
