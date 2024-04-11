2a02f4f2894975ad3c42831f34b540a5071e4ab8
yuklam <lamsungdev@gmail.com>
2024-04-11 12:32:31 +0800

    iter 50: add % in numpad

--- keyboards/crkbd/keymaps/lamsyl/keymap.c
+++ keyboards/crkbd/keymaps/lamsyl/keymap.c
@@ -187,7 +187,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB, KC_LEFT, KC_DOWN, KC_RGHT, KC_PPLS, KC_PMNS,                        KC_P4,   KC_P5,   KC_P6, KC_COLN, KC_SCLN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC_P1,   KC_P2,   KC_P3,  KC_DOT, KC_QUES, XXXXXXX,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC_P1,   KC_P2,   KC_P3,  KC_DOT, KC_QUES, KC_PERC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX,  KC_SPC,  KC_ENT,      KC_P0, KC_COMM, XXXXXXX
                                       //`--------------------------'  `--------------------------'
